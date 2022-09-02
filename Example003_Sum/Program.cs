5f34aca1ea5dbfc5b1fa450d54af781df3c03bac
Alexander Timirov <yudyymal@gmail.com>
2022-09-02 22:15:16 +0500

    Добавили Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 3;
-int NumberB = 5;
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int NumberB = new Random().Next(1,10);
+Console.WriteLine(NumberB);
 Console.WriteLine(numberA + NumberB);
--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double NumberA = 12;
+double numberB = 5;
+Console.WriteLine(NumberA / numberB);
