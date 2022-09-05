47cab819d90317192f8684a2319e34c00cd414bf
Alexander Timirov <yudyymal@gmail.com>
2022-09-05 19:03:01 +0500

    Добавили раздел 5 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string UserName = Console.ReadLine();
+
+if (UserName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(UserName);
+}
