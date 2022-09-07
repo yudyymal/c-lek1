e87cddd2fcda2d53bbbb22bec70b2351ebf32bcc
Alexander Timirov <yudyymal@gmail.com>
2022-09-07 20:39:09 +0500

    Добавили задачи ещё со второй лекции

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 10;
+int b1 = 5;
+int c1 = 55;
+int a2 = 110;
+int b2 = 95;
+int c2 = 25;
+int a3 = 210;
+int b3 = 25;
+int c3 = 395;
+
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+
+
+int max = Max(
+    Max(a1, b1, c1), 
+    Max(a2, b2, c2), 
+    Max(a3, b3, c3));
+
+
+Console.WriteLine(max);
---
+++ Example010_Method/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 12, 654, 123, 21, 45, 3, 13, 13 };
+
+int n = array.Length;
+int find = 13;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index=index+1
+    index++;
+}
