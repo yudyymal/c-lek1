021a3e25f3aed64d80759f2fe5d19cb7f06e49da
Alexander Timirov <yudyymal@gmail.com>
2022-09-16 19:51:58 +0500

    HW 3

---
+++ Homework3/hw1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine();
+string numberReverse = String.Empty;
+
+for (int i = number.Length - 1; i >= 0; i--)
+{
+    numberReverse = numberReverse + number[i];
+}
+if (number == numberReverse)
+{
+    Console.WriteLine("Число - палиндром");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
---
+++ Homework3/hw3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число, а я выдам таблицу кубов для данного количества");
+double a = int.Parse(Console.ReadLine());
+for (int i = 1; i <= a; i++)
+{
+    Console.Write(Math.Pow(i, 3));
+    Console.Write("   ");
+}
